27c5e39f4f2628129fa2e4db7eb129c1ed9e6258
leeikjeun <dldlrwjs12@gmail.com>
2017-03-31 19:08:00 +0900

    add test case

--- test/ac/kr/jejunu/UserDaoTest.java
+++ test/ac/kr/jejunu/UserDaoTest.java
@@ -15,18 +15,19 @@ import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.nullValue;
 
 public class UserDaoTest {
     UserDao userDao;
 
-
     @Before
-    public void setuo(){
+    public void setup(){
         //daoFactory = new DaoFactory();
         //ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         ApplicationContext context = new GenericXmlApplicationContext("daoFactory.xml");
         userDao = context.getBean("userDao",UserDao.class);
     }
+    //before을 하면 테스트 케이스 하기전에 먼저 돌아감
 
     @Test
     public void get() throws SQLException, ClassNotFoundException {
@@ -56,6 +57,48 @@ public class UserDaoTest {
         assertThat(id, is(resultUser.getId()));
         assertThat(name, is(resultUser.getName()));
         assertThat(password, is(resultUser.getPassword()));
+    }
+
+    @Test
+    public void update() throws SQLException, ClassNotFoundException {
+        String name = "lllqwe";
+        String password = "1234";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        Long id = userDao.add(user);
+        //UserDao userDao = daoFactory.UserDao();
+
+        String chageName = "testname";
+        String chagepass = "testpass";
+
+        user.setId(id);
+        user.setName(chageName);
+        user.setPassword(chagepass);
+
+        userDao.update(user);
+
+        User changeUser = userDao.get(id);
+
+        assertThat(id, is(changeUser.getId()));
+        assertThat(chageName, is(changeUser.getName()));
+        assertThat(chagepass, is(changeUser.getPassword()));
+    }
+
+    @Test
+    public void delate() throws SQLException, ClassNotFoundException {
+        String name = "lllqwe";
+        String password = "1234";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        Long id = userDao.add(user);
+
+        userDao.delete(id);
+
+        User deleteUser = userDao.get(id);
+
+        assertThat(deleteUser, nullValue());
     }
 
     @Test
